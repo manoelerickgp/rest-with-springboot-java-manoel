44a11311655916f0f77095ad73b4e28137db3642
manoelerickgp <manoel.computacao@gmail.com>
2023-09-20 19:18:06 -0300

    Projeto criado

---
+++ src/main/java/br/com/projeto/restwithspringbootjavamanoel/RestWithSpringbootJavaManoelApplication.java
@@ -0,0 +1,13 @@
+package br.com.projeto.restwithspringbootjavamanoel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithSpringbootJavaManoelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithSpringbootJavaManoelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/projeto/restwithspringbootjavamanoel/RestWithSpringbootJavaManoelApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projeto.restwithspringbootjavamanoel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringbootJavaManoelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
